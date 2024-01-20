b74fe8ad4e3ab4a3fcd0ff57e7d2ba4d31ee2a30
root <nourisoukaina11@gmail.com>
2024-01-20 12:50:19 -0800

    comp

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+'''async_generator
+'''
 import asyncio
-from random import uniform
-from typing import AsyncGenerator
+import random
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield uniform(0, 10)
-
+        yield random.random() * 10
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-'''
-Import async_generator from the previous task and then write a coroutine\
-    called async_comprehension that takes no arguments.
-
-The coroutine will collect 10 random numbers using an async comprehensing\
-    over async_generator, then return the 10 random numbers.
+'''async_comprehension
 '''
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
+from importlib import import_module as using
+
+
+async_generator = using('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
+    '''Creates a list of 10 numbers from generator.
     '''
-    collects and returns 10 random numbers using an async comprehension
-    '''
-    return [_ async for _ in async_generator()]
+    return [num async for num in async_generator()]
